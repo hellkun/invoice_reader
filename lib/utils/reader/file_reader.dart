934ae5d3811a5f5094526677caff8da9a2e96eff
hellkun <imrk@qq.com>
2021-07-10 10:06:54 +0800

    organize imports

--- lib/utils/reader/file_reader.dart
+++ lib/utils/reader/file_reader.dart
@@ -1,7 +1,6 @@
-import 'dart:typed_data';
 
 import 'package:invoice_reader/model/invoice.dart';
-import 'package:native_pdf_renderer/native_pdf_renderer.dart';
+
 import 'file_reader_web.dart' if (dart.library.io) 'file_reader_io.dart';
 
 abstract class InvoiceReader {
