bdb95eb1a7dc668b09b6ee635dcba14d9baafbdf
hellkun <imrk@qq.com>
2021-07-12 09:46:42 +0800

    update UI of ChangesScreen

--- lib/pages/changes_screen.dart
+++ lib/pages/changes_screen.dart
@@ -11,23 +11,45 @@ class ChangesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(24.0),
-      child: FutureBuilder<String>(
-        future: rootBundle.loadString(FileConstants.assetChangesPath),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return SingleChildScrollView(
-              child: MarkdownBody(data: snapshot.data!),
-            );
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text('加载更新文件异常:\n${snapshot.error}'),
-            );
-          }
+      child: Column(
+        children: [
+          Text(
+            '更新日志',
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          const Divider(),
+          Expanded(
+            child: FutureBuilder<String>(
+              future: rootBundle.loadString(FileConstants.assetChangesPath),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return SingleChildScrollView(
+                    child: MarkdownBody(data: snapshot.data!),
+                  );
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text('加载更新文件异常:\n${snapshot.error}'),
+                  );
+                }
 
-          return Center(
-            child: const CircularProgressIndicator(),
-          );
-        },
+                return Center(
+                  child: const CircularProgressIndicator(),
+                );
+              },
+            ),
+          ),
+
+          // 按钮区
+          ButtonBar(
+            layoutBehavior: ButtonBarLayoutBehavior.padded,
+            children: [
+              TextButton(
+                onPressed: Navigator.of(context).pop,
+                child: const Text('关闭'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
