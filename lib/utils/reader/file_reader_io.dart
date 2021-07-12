489e967822472533481c29808e251bedcd74dff5
hellkun <imrk@qq.com>
2021-07-12 17:39:31 +0800

    implement io

--- lib/utils/reader/file_reader_io.dart
+++ lib/utils/reader/file_reader_io.dart
@@ -1,17 +1,25 @@
 import 'dart:async';
 import 'dart:io';
-import 'dart:typed_data';
 
 import 'package:invoice_reader/model/invoice.dart';
 
 import 'file_reader.dart';
 
 class InvoiceReaderImpl implements InvoiceReader {
-  
   @override
-  Future<InvoiceSource> read(source) {
-    // TODO: implement read
-    throw UnimplementedError();
+  Future<InvoiceSource> read(source) async {
+    print('source = $source');
+    final file = File(source);
+    final bytes = await file.readAsBytes();
+
+    final fName = file.path.split('/').last;
+
+    final fExtName = fName.split('.').last;
+
+    return InvoiceSource(
+      bytes,
+      name: fName,
+      type: fExtName == 'pdf' ? InvoiceSourceType.pdf : InvoiceSourceType.image,
+    );
   }
-  
 }
