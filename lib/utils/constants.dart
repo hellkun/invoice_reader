813370a244ff4e9223b1e894f718bb897e8392bc
hellkun <imrk@qq.com>
2021-07-13 09:44:42 +0800

    fix path issues on non-web platforms

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/foundation.dart';
+
 /// Constants of files and assets, etc.
 class FileConstants {
+  static const _kPlatformAssetPath = kIsWeb ? '' : 'assets/';
+
   /// 更新日志的路径
-  static const assetChangesPath = 'CHANGES.md';
+  static const assetChangesPath = _kPlatformAssetPath + 'CHANGES.md';
 
   /// Excel模板文件的路径
-  static const xlsTemplatePath = 'xls_template.xlsx';
+  static const xlsTemplatePath = _kPlatformAssetPath + 'xls_template.xlsx';
 }
