f18798d0d43d6d683848460174c93e8e37077f09
hellkun <imrk@qq.com>
2021-07-17 01:24:35 +0800

    fix path issues

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,8 +1,6 @@
-import 'package:flutter/foundation.dart';
-
 /// Constants of files and assets, etc.
 class FileConstants {
-  static const _kPlatformAssetPath = kIsWeb ? '' : 'assets/';
+  static const _kPlatformAssetPath = 'assets/';
 
   /// 更新日志的路径
   static const assetChangesPath = _kPlatformAssetPath + 'CHANGES.md';
