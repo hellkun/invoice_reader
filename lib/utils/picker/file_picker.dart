453c8bf762f9e3d060cf4e6253cfdf6670e3a92c
hellkun <imrk@qq.com>
2021-07-23 10:51:04 +0800

    export XFile

--- lib/utils/picker/file_picker.dart
+++ lib/utils/picker/file_picker.dart
@@ -1,5 +1,8 @@
 import 'package:cross_file/cross_file.dart';
+
 import 'file_picker_io.dart' if (dart.library.html) 'file_picker_selector.dart';
+
+export 'package:cross_file/cross_file.dart';
 
 abstract class FilePicker {
   static FilePicker? _instance;
